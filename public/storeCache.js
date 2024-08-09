5a4e8cc481995b79760a1a2714c42be56fa8d1da
yisak mebrate <yisakmebratedesalegn@gmail.com>
2024-08-09 11:47:37 +0300

    ts2

--- public/storeCache.js
+++ public/storeCache.js
@@ -1,11 +1,6 @@
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/bundle.js',
-  '/static/js/1.chunk.js',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
+  '/'
 ];
 
 const serviceWorker = self;
