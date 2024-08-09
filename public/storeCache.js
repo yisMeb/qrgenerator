25b2de16a53c9da3e30fe3038eca6894f3d8929c
yisak mebrate <yisakmebratedesalegn@gmail.com>
2024-08-09 11:40:51 +0300

    test

--- public/storeCache.js
+++ public/storeCache.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'my-cache-v1';
-let urlsToCache = [
+const urlsToCache = [
   '/',
+  '/index.html',
   '/static/js/bundle.js',
   '/static/js/1.chunk.js',
   '/static/js/main.chunk.js',
